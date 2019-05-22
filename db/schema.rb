29ed773c83a4dd347214708654d30e4ba6ac07ea
Anhelina <anhelina2207@gmail.com>
2019-05-22 19:21:57 +0300

    work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190521155244) do
+ActiveRecord::Schema.define(version: 20190521222115) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 20190521155244) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_bookmarks_on_course_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "courses", force: :cascade do |t|
